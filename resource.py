f6fe37febe4106fd2af9ccdc8149668caecaeb0b
Rohan Dayaram <rohanday4@gmail.com>
2022-08-27 11:33:47 +0200

    resource

--- resource.py
+++ resource.py
@@ -8,5 +8,23 @@ class ResourceType(Enum):
 class Resource:
     def __init__(self, type, quantity, locations=[]) -> None:
         self.type = type
-        self.locations = locations
+        self.items = self.generateItems(locations)
         self.quantity = quantity
+        self.closest = self.getClosest(self.items)
+
+    def generateItems(self,locations):
+        for location in locations:
+            self.items.append(Item(location[0], location[1]))
+        return self.items
+
+    def getClosest(self,items):
+        closest = items[0]
+        for item in items:
+            if item.distance < closest.distance:
+                closest = item
+        return closest
+
+class Item:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
